40d184266470d6c5b204e4fbcb0a3d36746a8ee4
unknown <you@example.com>
2021-02-08 22:58:36 +0530

    todays execution

--- cityward-crud-operation-project/src/com/xworkz/xworkzapp/constants/WardName.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.xworkzapp.constants;
-
-public enum WardName {
-	BASAVESHWARA,GANDHINAGAR,RAJAJINAGAR,VIJAYANAGAR;
-
-}
--- hotel-crud-project/src/com/xworkz/xworkzapp/constants/Location.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.xworkzapp.constants;
-
-public enum Location {
-	VVPURAM,BASAVANGUDI,RAJAJINAGAR,MALLESHWARAM;
-
-}
